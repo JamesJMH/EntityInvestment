d7b39b6639e96ba16f593e8ddafa882c984ceb07
sijuntan <sijuntan@yeah.net>
2018-03-24 20:09:13 -0400

    .

--- EntityInvestment/views.py
+++ EntityInvestment/views.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def company_list(request):
+    return
+
+def investor_info(request):
+    return
+
+def company_info(request):
+    return
