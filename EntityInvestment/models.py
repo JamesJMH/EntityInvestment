7f102263d23dc7bd325b92be76fb0120b6d9693d
sijuntan <sijuntan@yeah.net>
2018-03-24 20:33:19 -0400

    update models

--- EntityInvestment/models.py
+++ EntityInvestment/models.py
@@ -5,6 +5,17 @@ from django.db import models
 
 # Create your models here.
 class Investor(models.Model):
+    account_name = models.CharField()
+    username = models.CharField()
+    password = models.CharField()
+    deposit = models.CharField()
 
 class Company(models.Model):
+    company_name = models.CharField()
+    description = models.CharField()
+    budget = models.IntegerField()
+    basic_info = models.CharField()
+    financial_info = models.CharField()
+    extra_info = models.CharField()
+
 
